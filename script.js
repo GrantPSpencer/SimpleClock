5016efc6ad959395844f351e59791cfb97a6ebd0
Grant Spencer <grantspencer95@gmail.com>
2021-03-11 21:57:09 -0500

    finishing touches

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
 'use strict'
 
-let date = new Date();
-// let time = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
 
-let hours = date.getHours();
-let minutes = date.getMinutes();
-let seconds = date.getSeconds();
-let period = 'am';
-
-if (hours > 12) {
-    hours -= 12;
-    period = 'pm';
-    hours = '0' + hours;
-}
-if (minutes < 10) {
-    minutes = '0' + minutes;
-}
-
-if (seconds < 10) {
-    seconds = '0' + seconds;
-}
 
 updateTime();
 
 let timerID = setInterval(() => {
-    seconds++;
     updateTime();
 }, 1000);
 
 
 
 function updateTime() {
+    let date = new Date();
+// let time = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let period = 'AM';
+
+    if (hours > 12) {
+        hours -= 12;
+        period = 'PM';
+        hours = '0' + hours;
+    }
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+
+    if (seconds < 10) {
+        seconds = '0' + seconds;
+    }
     let clock = document.getElementById('clock');
-    let time = hours + ':' + minutes + ':' + seconds + period;
+    let time = hours + ':' + minutes + ':' + seconds + ' ' + period;
     clock.innerHTML = time;
 }
